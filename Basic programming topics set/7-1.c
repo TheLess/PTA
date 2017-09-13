816e22b781bece333eb1248134354e43fb0a9d7f
Hao Sun <sh1029397604@qq.com>
2017-09-13 20:27:47 +0800

    Create 7-1.c

---
+++ Basic programming topics set/7-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int length = 0;
+	int foot = 0, inch = 0;
+	
+	scanf("%d", &length);
+	foot = length / 30.48;
+	inch = ((double)length/30.48 - foot)*12;
+	printf("%d %d", foot, inch);
+} 
