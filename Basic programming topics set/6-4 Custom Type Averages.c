08791db43c84226ad4f3a504270cc80fe29cd284
Hao Sun <sh1029397604@qq.com>
2017-09-14 08:37:44 +0800

    Create 6-4 Custom Type Averages.c

---
+++ Basic programming topics set/6-4 Custom Type Averages.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#define MAXN 10
+typedef float ElementType;
+
+ElementType Average( ElementType S[], int N );
+
+int main ()
+{
+    ElementType S[MAXN];
+    int N, i;
+
+    scanf("%d", &N);
+    for ( i=0; i<N; i++ )
+        scanf("%f", &S[i]);
+    printf("%.2f\n", Average(S, N));
+
+    return 0;
+}
+
+//answer
+ElementType Average( ElementType S[], int N )
+{
+	double sum = 0.0, avg = 0.0;
+	int i = 0;
+	
+	for( i = 0; i < N; i++ )
+	{
+		sum += S[i];
+	}
+	
+	avg = sum / N;
+	
+	return avg;
+}
