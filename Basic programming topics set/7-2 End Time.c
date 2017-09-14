1c0f3171668aa1f51435f526393cb0f30d323097
Hao Sun <sh1029397604@qq.com>
2017-09-14 19:41:36 +0800

    Create 7-2 End Time.c

---
+++ Basic programming topics set/7-2 End Time.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main( void )
+{
+	int start = 0, end = 0;
+	int start_min = 0, pass_min = 0, end_min = 0;
+	const int HTM = 60;
+	const int MTH = 100;
+	
+	scanf("%d %d", &start, &pass_min);
+	
+	start_min = start / 100 * HTM + start % 100;
+	end_min = start_min + pass_min;
+	end = (end_min) / 60 * MTH + end_min % 60;
+	
+	printf("%d\n", end);
+}
