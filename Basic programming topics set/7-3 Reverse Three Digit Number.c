9262e26cc84573816a9ca191199ded18ee1b929f
Hao Sun <sh1029397604@qq.com>
2017-09-14 19:58:07 +0800

    Create 7-3 Reverse Three Digit Number.c

---
+++ Basic programming topics set/7-3 Reverse Three Digit Number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main( void )
+{
+	int n, a[3], r = 0;
+	int i;
+	
+	scanf("%d", &n);
+	
+	for(i = 0; i < 3; i++)
+	{
+		a[i] = n % 10;
+		n = n / 10;
+	}
+	
+	r = a[0] * 100 + a[1] * 10 + a[2];
+	
+	printf("%d\n", r); 
+}
