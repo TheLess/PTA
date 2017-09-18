0bded000132305a7d8481b00ac6bfd797938280a
Hao Sun <sh1029397604@qq.com>
2017-09-18 19:52:44 +0800

    Create 7-11 Calculation Water Fee.c

---
+++ Basic programming topics set/7-11 Calculation Water Fee.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	double x, y;
+	
+	scanf("%lf", &x);
+	
+	if(x <= 15)
+	{
+		y = 4 * x / 3;	
+	}
+	else
+	{
+		y = 2.5 * x - 17.5;
+	}
+	
+	printf("%0.2lf", y);
+	
+	return 0;
+}
